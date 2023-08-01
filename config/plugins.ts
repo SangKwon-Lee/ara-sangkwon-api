deb62b5a34b1ae7e7fcbd3a4139588b585cc83e1
SangKwon-Lee <sangkwon.lee@pxd.co.kr>
2023-08-01 14:59:14 +0900

    depoly

--- config/plugins.ts
+++ config/plugins.ts
@@ -3,10 +3,10 @@ module.exports = ({ env }) => ({
     enabled: true,
     config: {
       deployHook:
-        "https://api.vercel.com/v1/integrations/deploy/prj_<deploy-hook>",
+        "https://api.vercel.com/v1/integrations/deploy/prj_ZyKVjdpVoP9LJKXhb4TQzLB2OIyN/tyzk90sI2q",
       apiToken: "ACrPx7Meg9R3CFzIsa62ZZDY",
-      appFilter: "your-app-name-on-vercel",
-      teamFilter: "your-team-id-on-vercel",
+      appFilter: "ara-sangkwon-api",
+      teamFilter: "ara-sangkwon-api",
       roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
     },
   },
